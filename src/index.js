43f352be53e5a788cb4bce8cf128e7c0b91205c6
Vadim Tissen <vadim.tisse@gmail.com>
2023-01-10 14:43:51 +0600

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let result = n.toString().split("").reverse().join("");
+    return result;
+};
